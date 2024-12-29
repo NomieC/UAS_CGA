aaa36d6bcc033415c5486e0b98f4682123deaae4
NomieC <118948158+NomieC@users.noreply.github.com>
2024-12-30 00:09:46 +0700

    add new feature

--- Assets/Codes/Player/PlayerCollector.cs
+++ Assets/Codes/Player/PlayerCollector.cs
@@ -1,20 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 public class PlayerCollector : MonoBehaviour
 {
-    CharacterStatus player;          // Reference to player stats
-    SphereCollider playerCollector;  // 3D sphere collider for magnet effect
-    public float magnetStrength; // Strength of the magnet effect
+    CharacterStatus player;          
+    SphereCollider playerCollector;  
+    public float magnetStrength = 10f;  // Strength of the magnet effect
+    public float stopDistance = 1f;     // Distance at which collectible stops moving towards player
+
+    private HashSet<Rigidbody> attractedObjects = new HashSet<Rigidbody>();
 
     void Start()
     {
-        // Get the CharacterStatus and SphereCollider components
         player = FindObjectOfType<CharacterStatus>();
         playerCollector = GetComponent<SphereCollider>();
-
-        // Ensure the collider acts as a trigger
-        playerCollector.isTrigger = true;
+        playerCollector.isTrigger = true;  // Ensure the collider acts as a trigger
     }
 
     void Update()
@@ -24,19 +25,59 @@ public class PlayerCollector : MonoBehaviour
         {
             playerCollector.radius = player.currentMagnetRange;
         }
+
+        // Apply force to all collected objects to pull them continuously
+        AttractCollectibles();
     }
 
-    // Detect collectibles that enter the magnet radius
+    private void AttractCollectibles()
+    {
+        foreach (Rigidbody rb in attractedObjects)
+        {
+            if (rb != null)
+            {
+                Vector3 direction = (transform.position - rb.position).normalized;
+                float distance = Vector3.Distance(transform.position, rb.position);
+
+                if (distance > stopDistance)
+                {
+                    rb.AddForce(direction * magnetStrength * Time.deltaTime, ForceMode.VelocityChange);
+                }
+                else
+                {
+                    // Collect the item when close enough
+                    if (rb.TryGetComponent(out ICollectible collectible))
+                    {
+                        collectible.Collect();
+                        attractedObjects.Remove(rb);
+                        Destroy(rb.gameObject);  // Optionally destroy the collectible after collection
+                    }
+                }
+            }
+        }
+    }
+
     private void OnTriggerEnter(Collider col)
     {
-        // Check if the object implements ICollectible
         if (col.gameObject.TryGetComponent(out ICollectible collectible))
         {
             Rigidbody rb = col.gameObject.GetComponent<Rigidbody>();
-            Vector3 direction = (transform.position - col.transform.position).normalized;
-            rb.AddForce(direction * magnetStrength, ForceMode.Impulse);
-            // Collect the item
-            collectible.Collect();
+            if (rb != null)
+            {
+                attractedObjects.Add(rb);
+            }
         }
     }
+
+    // private void OnTriggerExit(Collider col)
+    // {
+    //     if (col.gameObject.TryGetComponent(out ICollectible collectible))
+    //     {
+    //         Rigidbody rb = col.gameObject.GetComponent<Rigidbody>();
+    //         if (rb != null && attractedObjects.Contains(rb))
+    //         {
+    //             attractedObjects.Remove(rb);  // Stop attracting if it leaves the magnet radius
+    //         }
+    //     }
+    // }
 }
