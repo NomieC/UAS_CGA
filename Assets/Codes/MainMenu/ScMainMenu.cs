943a401cae580e54ae5dd3eb035acf823ba3ddc5
NomieC <118948158+NomieC@users.noreply.github.com>
2025-01-04 19:19:23 +0700

    Fidel gacor wak

--- Assets/Codes/MainMenu/ScMainMenu.cs
+++ Assets/Codes/MainMenu/ScMainMenu.cs
@@ -2,16 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 public class ScMainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadSceneAsync(1);
+        SceneManager.LoadSceneAsync(1);  // Memuat scene dengan index 1
     }
 
     public void QuitGame()
     {
-        // Application.Quit();
-        UnityEditor.EditorApplication.isPlaying = false;
+        #if UNITY_EDITOR
+        // Berhenti bermain saat di editor
+        EditorApplication.isPlaying = false;
+        #else
+        // Keluar dari game saat di build player
+        Application.Quit();
+        #endif
     }
 }
